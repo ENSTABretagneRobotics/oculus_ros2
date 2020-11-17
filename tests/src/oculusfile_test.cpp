8f6774de6089d08d4d8a0a01b70436c1417ead76
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-11-17 13:49:01 +0100

    [tests] Added .oculus file parsing test

---
+++ tests/src/oculusfile_test.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+#include <narval_base/files.h>
+using namespace narval::files;
+
+#include <narval_oculus/Oculus.h>
+#include <narval_oculus/print_utils.h>
+using namespace narval::oculus;
+
+const uint16_t oculusId = 0x4f53;
+const uint16_t deviceId = 7915;
+
+bool is_valid(const OculusMessageHeader& header)
+{
+    return header.oculusId == oculusId && header.srcDeviceId == deviceId;
+}
+
+bool find_header(std::ifstream& f, OculusMessageHeader& header)
+{
+    int counter = 0;
+    while(f.read(reinterpret_cast<char*>(&header), 4) && !is_valid(header))
+        counter += 4;
+    if(!f)
+        return false;
+
+    f.read(reinterpret_cast<char*>(&header) + 4, sizeof(header) - 4);
+
+    //cout << "Found header at byte : " << counter << endl;
+    //cout << header << endl;
+
+    return true;
+}
+
+void process(const OculusSimplePingResult* ping)
+{
+    cout << *ping << endl;
+}
+
+void parse(const std::vector<char>& buffer)
+{
+    switch((reinterpret_cast<const OculusMessageHeader*>(buffer.data()))->msgId)
+    {
+        case messageSimplePingResult:
+            process(reinterpret_cast<const OculusSimplePingResult*>(buffer.data()));
+            break;
+        case messageSimpleFire: cout << "Got simpleFire"   << endl; break;
+        case messagePingResult: cout << "Got pingResult"   << endl; break;
+        case messageUserConfig: cout << "Got userConfig"   << endl; break;
+        case messageDummy:      cout << "Got messageDummy" << endl; break;
+        default:
+            cout << "unknown message type" << endl;
+            break;
+    }
+}
+
+void read_messages(std::ifstream& f)
+{
+    OculusMessageHeader header;
+    std::vector<char> buffer;
+    
+    while(find_header(f, header)) {
+        buffer.resize(header.payloadSize + sizeof(header));
+        *(reinterpret_cast<OculusMessageHeader*>(buffer.data())) = header;
+        f.read(buffer.data() + sizeof(header), header.payloadSize);
+        parse(buffer);
+    }
+}
+
+int main()
+{
+    auto path = find_one(".*.oculus", std::string(std::getenv("HOME")) + "/Documents");
+    cout << "path : " << path << endl;
+
+    std::ifstream f(path, std::ios::binary);
+    if(!f.is_open())
+        throw std::runtime_error("Could not open file : " + path);
+    
+    read_messages(f);
+
+    return 0;
+}
