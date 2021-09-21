56e804f4db9b9898ae4c196cc69fa6803ff4f335
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2021-09-21 10:23:55 +0200

    Added license to all source files

--- src/AsyncService.cpp
+++ src/AsyncService.cpp
@@ -1,3 +1,21 @@
+/******************************************************************************
+ * narval_oculus driver library for Blueprint Subsea Oculus sonar.
+ * Copyright (C) 2020 ENSTA-Bretagne
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
 #include <narval_oculus/AsyncService.h>
 
 namespace narval { namespace oculus {
